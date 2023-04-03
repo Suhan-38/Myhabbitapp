1ccd72bfcae2f507a147fe59c5c68d5581b939f8
Suhan-38 <125673689+Suhan-38@users.noreply.github.com>
2023-04-03 13:39:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter,Routes,Route, HashRouter } from 'react-router-dom';
 import Home from './Pages/Home';
 import Addhabit from './Pages/Addhabit';
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/add' element={<Addhabit />} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
